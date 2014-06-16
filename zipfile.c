7354ffa64f89812592225f3640f3b88c670e74f2
mofei313 <xmbkdy313@gmail.com>
2014-06-16 16:37:09 +0800

    Add contant to zipfile.c

--- zipfile.c
+++ zipfile.c
@@ -1,11 +1,30 @@
-#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
 
-void zipfile() {
-	char command_str[256];
-
+int ZipFile(char *fileName, int zipFlag) {
+	char sCmd[256];
 	
+	if(zipFlag) {
+		if(0 == strncasecmp(fileName+strlen(fileName)-3,".gz",3))
+			return 0;
+		strcpy(sCmd, "gzip ");
+	} else {
+		if(0 != strncasecmp(fileName+strlen(fileName)-3,".gz",3))
+			return 0;
+		strcpy(sCmd,"gunzip ");
+	}
+	strcat(sCmd,fileName);
+
+	return system(sCmd);
 }
 
 int main(void) {
+	char *fileName="hello.c";
+	int zipFlag = 1;
+	
+	ZipFile(fileName,zipFlag);
 
+	return 0;
 }
+
